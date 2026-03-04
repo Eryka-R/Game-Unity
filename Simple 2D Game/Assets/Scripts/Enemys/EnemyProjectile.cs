d0f065195cc96ec2d88bf792a2a44be63c3dfb42
816778 <816778@unizar.es>
2026-03-04 17:15:17 +0100

    Traps

---
+++ Simple 2D Game/Assets/Scripts/Enemys/EnemyProjectile.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class EnemyProjectile : EnemyDamage
+{
+
+    [SerializeField] private float speed = 10;
+    [SerializeField] private float resetTime;
+    private float lifetime;
+
+    public void ActivateProjectile()
+    {
+        lifetime = 0;
+        gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        float movementSpeed = speed * Time.deltaTime;
+        transform.Translate(movementSpeed, 0, 0);
+
+        lifetime += Time.deltaTime;
+        if (lifetime > resetTime)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        base.OnTriggerEnter2D (collision);
+        print("Enemys Projectile hit: " + collision.gameObject.tag);
+        if (!collision.CompareTag("Heart"))
+        {
+            gameObject.SetActive(false);
+        }
+        
+
+    }
+}
+ 
